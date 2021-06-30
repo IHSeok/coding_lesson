61da1c04e9d907cbbd557ded7b39db67eafb6e07
IHSeok <82421757+IHSeok@users.noreply.github.com>
2021-06-30 17:43:02 +0900

    Game3

---
+++ practice.py
@@ -0,0 +1,14 @@
+# import math
+
+# rad_angle = math.radians(0)
+# print(rad_angle)
+
+# rad_angle = math.radians(90)
+# print(rad_angle)
+
+# rad_angle = math.radians(180)
+# print(rad_angle)
+
+
+import pygame
+print(pygame.font.get_fonts())
